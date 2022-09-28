b233c844b9bc7a4dc2d58486b1a8532b95e4bc10
TCdotmake <tommyjchen@hotmail.com>
2022-09-28 16:29:43 -0400

    checkGameEnd returning either token of winner, 0 for tie, null for active

--- index.js
+++ index.js
@@ -1,123 +1,165 @@
-function app(
-){
-    const setupFunctions = {
-        initialSetup: function(){
-            this.generateWinCondition();
-            this.newGame(); 
-        },
-        newGame: function(){
-            this.setupCells();
-            this.setupBoardData();
-            this.active = true;
-            this.winner = null;
-        },
-        setupCells: function(){
-            const gameBoard = document.getElementById('gameBoard');
-            //clear board if not empty
-            while(gameBoard.firstChild){
-                gameBoard.remove(gameBoard.lastChild);
-            }
-            for(let i = 0; i < 9; i++){
-                let cell = document.createElement('button');
-                cell.classList.add('cell');
-                cell.setAttribute('data-cell', i);
-                cell.addEventListener('click', e=>handleClick(e));
-                gameBoard.insertAdjacentElement("beforeend", cell);
-            }
-        },
-        setupBoardData: function(){
-            this.board = [];
-            for(let i = 0; i < 9; i++){this.board.push('');}
-        },
-    }
+function app() {
+  const setupFunctions = {
+    initialSetup: function () {
+      this.generateWinCondition();
+      this.newGame();
+    },
+    newGame: function () {
+      this.setupCells();
+      this.setupBoardData();
+      this.active = true;
+      this.winner = null;
+    },
+    setupCells: function () {
+      const gameBoard = document.getElementById("gameBoard");
+      //clear board if not empty
+      while (gameBoard.firstChild) {
+        gameBoard.remove(gameBoard.lastChild);
+      }
+      for (let i = 0; i < 9; i++) {
+        let cell = document.createElement("button");
+        cell.classList.add("cell");
+        cell.setAttribute("data-cell", i);
+        cell.addEventListener("click", (e) => handleClick(e));
+        gameBoard.insertAdjacentElement("beforeend", cell);
+      }
+    },
+    setupBoardData: function () {
+      this.board = [];
+      for (let i = 0; i < 9; i++) {
+        this.board.push("");
+      }
+    },
+  };
 
-    function handleClick(e){
-        ticTacToe.executeMove(e.target.dataset.cell);
-    }
+  function handleClick(e) {
+    ticTacToe.executeMove(e.target.dataset.cell);
+  }
 
-    const gameFlowFunctions = {
-        nextTurn: function(){
-            if(this.currentToken === '1'){
-                this.currentToken = this.player_2.token;
-                this.currnetIcon = this.player_2.icon;
-            }else{
-                this.currentToken = this.player_1.token;
-                this.currnetIcon = this.player_1.icon;
-            }
-        },
-        executeMove: function(index){
-            if(this.board[index] === '' && this.active){
-                this.board[index] = this.currentToken;
-                const cells = document.querySelectorAll('.cell');
-                cells[index].insertAdjacentElement("beforeend", this.createIcon());
-                let result = this.checkForWin();
-                if(result!==null){this.gameEnd();}
-                if(this.active){
-                    this.nextTurn();
-                }
-            }
-        },
-    }
+  const aiFunctions = {
+    aiMove: function () {
+      let cell = 0;
+      return cell;
+    },
+    getScore: function (endResult, maximizerToken) {
+      if (endResult == 0) {
+        return endResult;
+      } else if (endResult === maximizerToken) {
+        return 1;
+      } else {
+        return -1;
+      }
+    },
+    minmax: function (board, depth, isMaxmizing, token) {
+      let result = this.checkGameEnd(board);
+      if (result !== null) {
+      }
+    },
+  };
 
-    const gameEndMethods = {
-        gameEnd: function(){this.active = false;},
-        matchThree: function(a, b, c){
-            let result = null;
-            if(a===b && b===c && a!==''){
-                result = a;
-            }
-            return result;
-        },
-        generateWinCondition: function(){
-            this.winCondition = [];
-            //rows
-            for(let i =0; i<9; i+=3){
-                this.winCondition.push([i, i+1, i+2]);
-            }
-            //columns
-            for(let i=0; i<3; i++){
-                this.winCondition.push([i, i+3, i+6]);
-            }
-            //diagonal
-            this.winCondition.push([0,4,8]);
-            this.winCondition.push([2,4,6]);
-        },
-        checkForWin: function(){
-            for(cell of this.winCondition){
-                let result = this.matchThree(this.board[cell[0]], this.board[cell[1]], this.board[cell[2]]);
-                if(result!==null){return result;}
-            }
-            return null;
+  const gameFlowFunctions = {
+    nextTurn: function () {
+      if (this.currentToken === "1") {
+        this.currentToken = this.player_2.token;
+        this.currnetIcon = this.player_2.icon;
+      } else {
+        this.currentToken = this.player_1.token;
+        this.currnetIcon = this.player_1.icon;
+      }
+    },
+    executeMove: function (index) {
+      if (this.board[index] === "" && this.active) {
+        this.board[index] = this.currentToken;
+        const cells = document.querySelectorAll(".cell");
+        cells[index].insertAdjacentElement("beforeend", this.createIcon());
+        let result = this.checkGameEnd(this.board);
+        if (result !== null) {
+          this.gameEnd();
         }
-    }
-
-    const displayFunctions = {
-        createIcon: function(){
-            const newIcon = document.createElement('i');
-            newIcon.classList.add('bi');
-            newIcon.classList.add(this.currnetIcon);
-            return newIcon;
+        if (this.active) {
+          this.nextTurn();
         }
-    }
+      }
+    },
+  };
 
-    function createPlayer(token, icon){
-        return{token, icon};
-    }
-
-    const ticTacToe = (function(){
-        return{
-            player_1: createPlayer('1', 'bi-x-lg'),
-            player_2: createPlayer('2', 'bi-circle'),
-            currentToken: '1',
-            currnetIcon: 'bi-x-lg',
-            active: true,
-            ...setupFunctions,
-            ...gameFlowFunctions,
-            ...displayFunctions,
-            ...gameEndMethods,
+  const gameEndMethods = {
+    gameEnd: function () {
+      this.active = false;
+    },
+    matchThree: function (a, b, c) {
+      let result = null;
+      if (a === b && b === c && a !== "") {
+        result = a;
+      }
+      return result;
+    },
+    generateWinCondition: function () {
+      this.winCondition = [];
+      //rows
+      for (let i = 0; i < 9; i += 3) {
+        this.winCondition.push([i, i + 1, i + 2]);
+      }
+      //columns
+      for (let i = 0; i < 3; i++) {
+        this.winCondition.push([i, i + 3, i + 6]);
+      }
+      //diagonal
+      this.winCondition.push([0, 4, 8]);
+      this.winCondition.push([2, 4, 6]);
+    },
+    checkForWin: function (board) {
+      for (cell of this.winCondition) {
+        let result = this.matchThree(
+          board[cell[0]],
+          board[cell[1]],
+          board[cell[2]]
+        );
+        if (result !== null) {
+          return result;
         }
-    })();
-    ticTacToe.initialSetup();
-    console.log(ticTacToe.winCondition);
+      }
+      return null;
+    },
+    checkGameEnd: function (board) {
+      let result = this.checkForWin(board);
+      if (result !== null) {
+        //do nothing
+      } else if (!board.includes("")) {
+        result = 0;
+      } else result = null;
+      console.log(result);
+      return result;
+    },
+  };
+
+  const displayFunctions = {
+    createIcon: function () {
+      const newIcon = document.createElement("i");
+      newIcon.classList.add("bi");
+      newIcon.classList.add(this.currnetIcon);
+      return newIcon;
+    },
+  };
+
+  function createPlayer(token, icon) {
+    return { token, icon };
+  }
+
+  const ticTacToe = (function () {
+    return {
+      player_1: createPlayer("1", "bi-x-lg"),
+      player_2: createPlayer("2", "bi-circle"),
+      currentToken: "1",
+      currnetIcon: "bi-x-lg",
+      active: true,
+      ...setupFunctions,
+      ...gameFlowFunctions,
+      ...displayFunctions,
+      ...gameEndMethods,
+      ...aiFunctions,
+    };
+  })();
+  ticTacToe.initialSetup();
 }
 app();
