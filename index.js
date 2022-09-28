9dd6d54907906de2bf0315d26f8daec5a907da40
TCdotmake <tommyjchen@hotmail.com>
2022-09-28 01:53:17 -0400

    generateWinCondition at startup

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function app(
 ){
     const setupFunctions = {
+        initialSetup: function(){
+            this.generateWinCondition();
+            this.newGame(); 
+        },
+        newGame: function(){
+            this.setupCells();
+            this.setupBoardData();
+            this.active = true;
+        },
         setupCells: function(){
             const gameBoard = document.getElementById('gameBoard');
             //clear board if not empty
@@ -11,15 +20,86 @@ function app(
                 let cell = document.createElement('button');
                 cell.classList.add('cell');
                 cell.setAttribute('data-cell', i);
+                cell.addEventListener('click', e=>handleClick(e));
                 gameBoard.insertAdjacentElement("beforeend", cell);
             }
         },
+        setupBoardData: function(){
+            this.board = [];
+            for(let i = 0; i < 9; i++){this.board.push('');}
+        },
     }
+
+    function handleClick(e){
+        ticTacToe.executeMove(e.target.dataset.cell);
+        console.log(ticTacToe.board);
+    }
+
+    const gameFlowFunctions = {
+        nextTurn: function(){
+            if(this.currentToken === '1'){
+                this.currentToken = this.player_2.token;
+                this.currnetIcon = this.player_2.icon;
+            }else{
+                this.currentToken = this.player_1.token;
+                this.currnetIcon = this.player_1.icon;
+            }
+        },
+        executeMove: function(index){
+            if(this.board[index] === '' && this.active){
+                this.board[index] = this.currentToken;
+                const cells = document.querySelectorAll('.cell');
+                cells[index].insertAdjacentElement("beforeend", this.createIcon());
+                this.nextTurn();
+            }
+        },
+    }
+
+    const gameEndMethods = {
+        matchThree: function(){},
+        generateWinCondition: function(){
+            this.winCondition = [];
+            //rows
+            for(let i =0; i<9; i+=3){
+                this.winCondition.push([i, i+1, i+2]);
+            }
+            //columns
+            for(let i=0; i<3; i++){
+                this.winCondition.push([i, i+3, i+6]);
+            }
+            //diagonal
+            this.winCondition.push([0,4,8]);
+            this.winCondition.push([2,4,6]);
+        }
+    }
+
+    const displayFunctions = {
+        createIcon: function(){
+            const newIcon = document.createElement('i');
+            newIcon.classList.add('bi');
+            newIcon.classList.add(this.currnetIcon);
+            return newIcon;
+        }
+    }
+
+    function createPlayer(token, icon){
+        return{token, icon};
+    }
+
     const ticTacToe = (function(){
         return{
+            player_1: createPlayer('1', 'bi-x-lg'),
+            player_2: createPlayer('2', 'bi-circle'),
+            currentToken: '1',
+            currnetIcon: 'bi-x-lg',
+            active: true,
             ...setupFunctions,
+            ...gameFlowFunctions,
+            ...displayFunctions,
+            ...gameEndMethods,
         }
     })();
-    ticTacToe.setupCells();
+    ticTacToe.initialSetup();
+    console.log(ticTacToe.winCondition);
 }
 app();
